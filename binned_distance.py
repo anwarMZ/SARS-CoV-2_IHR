236e829045f47d4e6f4c3d06b2052995c00a9106
Muhammad Zohaib Anwar <mzanwar@envs.au.dk>
2021-01-22 00:59:48 -0800

    Adding Min-Max limits to bins

--- binned_distance.py
+++ binned_distance.py
@@ -26,17 +26,23 @@ if __name__ == '__main__':
 
     out_dates = []
     out_dist = []
+    out_min = []
+    out_max = []
 
     while end_date <= max(metadata['date']):
         sub_meta = metadata.query('date >= @start_date and date <= @end_date')
         ids = sub_meta['strain']
         sub_dist = dist_mat[dist_mat['strain'].isin(ids)]
         avg_dist = sub_dist['distance'].mean()
+        min_dist = sub_dist['distance'].min()
+        max_dist = sub_dist['distance'].max()
         out_dates.append('{0} - {1}'.format(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))
         out_dist.append(avg_dist)
+        out_min.append(min_dist)
+        out_max.append(max_dist)
 
         start_date += pd.DateOffset(days=1)
         end_date += pd.DateOffset(days=1)
 
-    out_df = pd.DataFrame({'date_range': out_dates, 'average_distance': out_dist})
+    out_df = pd.DataFrame({'date_range': out_dates, 'average_distance': out_dist, 'min_distance': out_min, 'max_distance': out_max})
     out_df.to_csv(args.out, sep='\t', index=False)
